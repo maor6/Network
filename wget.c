70a2f6ebf85712db9e5f874daf96b8e6e73821b7
gofna <gofnaivry90@gmail.com>
2020-01-02 19:08:50 +0200

    last fixes

--- wget.c
+++ wget.c
@@ -1,100 +1,184 @@
-/*
- ============================================================================
- Name        : wget.c
- Author      : Dekel
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
 
-#include <stdio.h>
-#include <stdlib.h>
-
-#include <unistd.h>
+#include <sys/types.h>
+#include <unistd.h>          
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>    
 #include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
 
-#include "url_parser.h"
-#define MESSAGE_LENGTH 8192
+#define SIM_LENGTH 10 
 
-int main(int argc, char *argv[])
+
+typedef struct url {
+    char* protocol;
+    char* hostname;
+    unsigned short port;
+    char* path;
+} url;
+
+struct sockaddr_in resolve_address(char* hostname)
 {
-  int sock;
-  struct sockaddr_in cli_name;
+  struct addrinfo* res;
+  //char* hostaddr;
+  struct sockaddr_in* saddr;
+  
+  
+    if (0 != getaddrinfo(hostname, NULL, NULL, &res)) {
+      fprintf(stderr, "Error in resolving hostname %s\n", hostname);
+      exit(1); // temp
+    }
 
-  //Check that the user inserted exactly command line argument.
-  if (argc != 2)
-  {
-    perror("Usage:\n1. <protocol>://<hostname>/<path>\n2. <protocol>://<hostname>:<port>/<path>\n");
+    // addrinfo.ai_addr can be casted into sockaddr or sockaddr_in
+  saddr = (struct sockaddr_in*)res->ai_addr;
+  //coverting between endians.
+ // hostaddr = inet_ntoa(saddr->sin_addr);
+ return *saddr;
+}
+
+// PARSING <protocol>://<hostname>:<port>/<path>
+// or
+// PARSING <protocol>://<hostname>/<path>
+struct url parse_url(char* source) 
+{
+    url u;
+    int len = strlen(source);
+    int i   = 0; // index on source
+    int k   = 0; // index on HostName or path inside struct of url
+    unsigned short port= 0; // decimal value of parsed string for "port"
+    char c  = *source;
+    
+    // parse PROTOCOL
+    u.protocol = (char*) malloc(len*sizeof(char));
+    while(c != ':' && i < len)
+    {
+        *(u.protocol+i) = c;
+        c = *(source+(++i));
+    }
+    *(u.protocol+i) = '\0';
+    
+    i+=3; //advance TODO: validate existance of "//""
+    c = *(source+(i));
+    
+    // parse HOSTNAME
+    u.hostname = (char*) malloc(len*sizeof(char));
+    while(c != ':' && c != '/' && i < len)
+    {
+        *(u.hostname+(k++)) = c;
+        c = *(source+(++i));
+    }
+    *(u.hostname+k) = '\0';
+    
+    // check type
+    // if port
+    if(c == ':') // port mode
+    {
+        c = *(source+(++i));
+        while(c != '/' && i < len)
+        {
+		    port = port * 10 + ( c - '0' );
+            c = *(source+(++i));
+        }
+        u.port = port;
+    } else u.port = 80;
+    
+    
+    k = 0; // reset pos
+    
+    u.path = (char*) malloc(len*sizeof(char));
+    c = *(source+(++i));
+    while(c != '/' && i < len)
+    {
+        *(u.path+(k++)) = c;
+        c = *(source+(++i));
+    }
+   *(u.path+k) = '\0';
+   if(strlen(u.path) == 0)
+        (u.path) = "/\0";
+   
+    return u;
+}
+
+
+int main(int argc, char* argv[])
+{ 
+  int sock; 
+  struct sockaddr_in cli_name, dest_address; 
+  int count;
+  int value; 
+
+    // if there is no host provided by the user in command line then exit.
+  if (argc != 2) {
+    perror("Usage: net_client <hostname>\n");
     exit(1);
   }
 
-  printf("Resolving %s\n", argv[1]);
+  // get hostname string from command line argument
+  char* hostname = argv[1];
+  // parse command line.
+  url u = parse_url(hostname);
+  printf("%s\n",hostname);
+  printf("Protocol: %s, Hostname: %s, Port: %hu, Path: %s\n", u.protocol, u.hostname, u.port, u.path);
 
-  //The system call creates a new socket(file descriptor). Every communication method starts with a socket.
+
+  // get IP address from HOST address
+  dest_address = resolve_address(u.hostname);
+  printf("%s\n",inet_ntoa(dest_address.sin_addr));
+  hostname = inet_ntoa(dest_address.sin_addr);
+
+  printf("Client is alive and establishing socket connection.\n");
+  
+  // create new TCP socket.
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0)
-    {
-	  perror ("Error opening channel\n");
+    { perror ("Error opening channel");
       close(sock);
       exit(1);
     }
-
-  struct timeval timeout;//Timeout for the connection
-  timeout.tv_sec = 10;
-  timeout.tv_usec = 0;
-  //Set send timeout
-  if (setsockopt (sock, SOL_SOCKET, SO_SNDTIMEO, (char *)&timeout,
-              sizeof(timeout)) < 0)
-  {
-	  perror ("Error setting socket timeout\n");
-	  close(sock);
-	  exit(1);
-  }
-
-  //Set receive timeout
-  if (setsockopt (sock, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
-              sizeof(timeout)) < 0)
-  {
-	  perror ("Error setting socket timeout\n");
-	  close(sock);
-	  exit(1);
-  }
-
-  url_t user_args;
-  parse_url(argv[1], &user_args);//Send the user input argument.
-
-  memset((char *)&cli_name,0, sizeof(cli_name));
-  cli_name.sin_family = AF_INET;//Yes we are using IPV4
-  cli_name.sin_addr.s_addr = user_args.ip.s_addr;//Copy the Internet address from result. address is already converted into network representation.
-  cli_name.sin_port = htons(user_args.port);//We also need to convert the PORT to the correct endianess.(big or little - this is transparent to us)
+      
+  // create a destination configuration with given port and host parsed by the parse_url function
+  bzero(&cli_name, sizeof(cli_name)); 
+  cli_name.sin_family = AF_INET; 
+  cli_name.sin_addr.s_addr = inet_addr(hostname); 
+  cli_name.sin_port = htons(u.port);
 
 
-  //This system call connects the socket to the server at IP 127.0.0.1 port 1337. Of course we uses IPV4
   if (connect(sock, (struct sockaddr *)&cli_name, sizeof(cli_name)) < 0)
-    {
-	  perror ("Error establishing communications\n");
+    { perror ("Error establishing communications");
       close(sock);
       exit(1);
     }
 
-  char message[MESSAGE_LENGTH];
-  snprintf(message, sizeof(message), "GET %s HTTP/1.0\r\nHost: %s\r\n\r\n", user_args.path, user_args.h_name);
-  char data [MESSAGE_LENGTH + 1];//+1 for null terminator
-  write(sock, message, sizeof(message));
+    /* construct an HTTP request: 
+    GET /PATH HTTP/1.0
+    Host: HOSTNAME
+    */
+   char* ch = (char*) malloc(sizeof(char)*80);
+   memset(ch, '\0', 80);
+   ch = strcat(ch,"GET ");
+   ch = strcat(ch,u.path);
+   ch = strcat(ch," HTTP/1.0\r\nHost: ");
+   ch = strcat(ch,u.hostname);
+   ch = strcat(ch,"\r\n\r\n");
 
-  ssize_t length = 0;
-  while((length = read(sock, data, sizeof(data) - 1)) > 0)
-  {
-	  data[length] = '\0';
-	  printf("%s", data);
-  }
-  printf("\nExiting now.\n");
+   printf("%s\nSize of request: %zu\n", ch, strlen(ch));
+   char* response = (char*) malloc(sizeof(char)*65550);
+   send(sock, ch, strlen(ch),0);
+    int i =0;
+    // read incoming data.
+    while(read(sock, (response+(i++)), 1)>0);
 
-  //This system call closes the file descriptor of our connection.
-  close(sock);
-  exit(1);
-}
+  printf("Client has received %s from socket.\n", response);
+  printf("Exiting now.\n");
+
+  close(sock); 
+  exit(0); 
+
+} 
